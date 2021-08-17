edd8c15e73cdb47a8b65614ae7d1ba9ff7015a2d
A601-20 <tnk.dat.class@gmail.com>
2021-08-17 19:43:50 +0900

    調整

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    float cameraDistance = 0;
+    Rigidbody rb = null;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraDistance = Vector3.Distance(Camera.main.transform.position, transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var mp = Input.mousePosition;
+        mp.z = cameraDistance;
+        var wp = Camera.main.ScreenToWorldPoint(mp);
+        var v = (wp - transform.position) / Time.fixedDeltaTime;
+        rb.velocity = v;
+    }
+
+
+}
